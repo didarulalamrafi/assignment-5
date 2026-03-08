da8fea5f37dead0505c023f5e4a019a708c175aa
Didarul Alam <didarulalamrafi1312@gmail.com>
2026-03-08 19:53:43 +0600

    open

--- script/all-issues.js
+++ script/all-issues.js
@@ -2,32 +2,31 @@
 
 // console.log(allIssuesCard)
 
-
 const allIssuesCard = document.getElementById("all-issues-card");
 
 const openIssuesbtn = document.getElementById("all-btn");
-openIssuesbtn.addEventListener("click", ()=>{
-const allIssues = () => {
-  const url = "https://phi-lab-server.vercel.app/api/v1/lab/issues";
-  fetch(url)
-    .then((res) => res.json())
-    .then((issues) => {
-      displayAllIssues(issues.data);
-    });
-};
+openIssuesbtn.addEventListener("click", () => {
+  const allIssues = () => {
+    const url = "https://phi-lab-server.vercel.app/api/v1/lab/issues";
+    fetch(url)
+      .then((res) => res.json())
+      .then((issues) => {
+        displayAllIssues(issues.data);
+      });
+  };
 
   const displayAllIssues = (issues) => {
-  issues.forEach((issues) => {
-    // console.log(issues)
-    const totalIssues = document.getElementById("total-issues");
-    totalIssues.innerText = issues.id;
-    const allIssuesCardcreate = document.createElement("div");
-    const borderTop =
-      issues.status === "open"
-        ? "border-t-4 border-green-600"
-        : "border-t-4 border-[#A855F7]";
+    issues.forEach((issues) => {
+      // console.log(issues)
+      const totalIssues = document.getElementById("total-issues");
+      totalIssues.innerText = issues.id;
+      const allIssuesCardcreate = document.createElement("div");
+      const borderTop =
+        issues.status === "open"
+          ? "border-t-4 border-green-600"
+          : "border-t-4 border-[#A855F7]";
 
-    allIssuesCardcreate.innerHTML = `
+      allIssuesCardcreate.innerHTML = `
                 <div id="card" class ="card shadow-lg h-[100%] w-[100%] ">
                     <div class="top-part p-4 rounded-md ${borderTop}">
                     <div class="top flex justify-between items-center">
@@ -49,66 +48,72 @@ const allIssues = () => {
                         <p class="text-[#64748B]">${issues.createdAt}</p>
                     </div>
                 </div>`;
-    allIssuesCard.append(allIssuesCardcreate);
-  });
-};
-allIssues();
-})
+      allIssuesCard.append(allIssuesCardcreate);
+    });
+  };
+  allIssues();
+});
 
 // All button end **************************
 
 // Open btn
 
 const openIssues = document.getElementById("open-btn");
-openIssues.addEventListener("click", ()=>{
-const openIssues = () => {
-  const url = "https://phi-lab-server.vercel.app/api/v1/lab/issues";
-  fetch(url)
-    .then((res) => res.json())
-    .then((issues) => {
-      displayOpenIssues(issues.data);
-    });
-};
+openIssues.addEventListener("click", () => {
+  const openIssues = () => {
+    const url = "https://phi-lab-server.vercel.app/api/v1/lab/issues";
+    fetch(url)
+      .then((res) => res.json())
+      .then((status) => {
+        displayOpenIssues(status.data);
+      });
+  };
 
-  const displayOpenIssues = (issues) => {
-  issues.forEach((issues) => {
-    // console.log(issues)
-    const totalIssues = document.getElementById("total-issues");
-    totalIssues.innerText = issues.id;
-    const openIssuesCardcreate = document.createElement("div");
-    // const borderTop =
-    //   issues.status === "open"
-    //     ? "border-t-4 border-green-600"
-    //     : "border-t-4 border-[#A855F7]";
+  const displayOpenIssues = (status) => {
+    // openIssuesCardcreate.innerHTML = "";
+    status.forEach((st) => {
+      if (st.status === "open") {
+        console.log(status);
+        const totalIssues = document.getElementById("total-issues");
 
-    openIssuesCardcreate.innerHTML = `
+        const openCount = status.filter((iss) => iss.status === "open").length;
+        totalIssues.innerText = openCount;
+
+        const openIssuesCardcreate = document.createElement("div");
+        const borderTop =
+          st.status === "open"
+            ? "border-t-4 border-green-600"
+            : "border-t-4 border-red-600";
+
+        openIssuesCardcreate.innerHTML = `
                 <div id="card" class ="card shadow-lg h-[100%] w-[100%] ">
-                    <div class="top-part p-4 rounded-md">
-                    <div class="top flex justify-between items-center">
+                    <div class="top-part p-4 rounded-md ${borderTop}">
+                    <div class="top flex justify-between items-center ">
                     
-                  <img src="${issues.status === "open" ? "./assets/Open-Status.png" : "./assets/Closed- Status .png"}" alt="">
+                  <img src="${st.status === "open" ? "./assets/Open-Status.png" : "./assets/Closed- Status .png"}" alt="">
 
-                    <button class="btn btn-soft bg-red-100 btn-error rounded-full text-red-500 px-6">${issues.priority}</button>  
+                    <button class="btn btn-soft bg-red-100 btn-error rounded-full text-red-500 px-6">${st.priority}</button>  
                     </div>
-                        <h4 class="font-semibold text-md">${issues.title}</h4>
-                        <p class="text-[#64748B]">${issues.description}</p>
+                        <h4 class="font-semibold text-md">${st.title}</h4>
+                        <p class="text-[#64748B]">${st.description}</p>
                         <div class="mt-3">
-                            <button class="flex items-center justify-center btn btn-soft bg-red-100 btn-error rounded-full text-red-500 "> <i class="fa-solid fa-bug"></i> ${issues.labels[0]} </button>
-                            <button class="flex items-center text-center justify-center btn btn-soft bg-yellow-100 btn-warning rounded-full text-yellow-500"><i class="fa-solid fa-circle-radiation"></i> ${issues.labels[1]} </button>
+                            <button class="flex items-center justify-center btn btn-soft bg-red-100 btn-error rounded-full text-red-500 "> <i class="fa-solid fa-bug"></i> ${st.labels[0] || " "} </button>
+                            <button class="flex items-center text-center justify-center btn btn-soft bg-yellow-100 btn-warning rounded-full text-yellow-500"><i class="fa-solid fa-circle-radiation"></i> ${st.labels[1] || " "} </button>
                         </div>
                     </div>
                     <div class="divider my-0"></div>
                     <div class="p-4 mt-0 space-y-2">
-                        <p class="text-[#64748B]">${issues.author}</p>
-                        <p class="text-[#64748B]">${issues.createdAt}</p>
+                        <p class="text-[#64748B]">${st.author}</p>
+                        <p class="text-[#64748B]">${st.createdAt}</p>
                     </div>
                 </div>`;
-    allIssuesCard.append(openIssuesCardcreate);
-  });
-};
-openIssues();
-})
-
+        allIssuesCard.append(openIssuesCardcreate);
+      }
+      // console.log(st);
+    });
+  };
+  openIssues();
+});
 
 // Closed btn ***************************************************
 
@@ -138,10 +143,10 @@ openIssues();
 //                 <div id="card" class ="card shadow-lg h-[100%] w-[100%] ">
 //                     <div class="top-part p-4 rounded-md">
 //                     <div class="top flex justify-between items-center">
-                    
+
 //                   <img src="${issues.status === "open" ? "./assets/Open-Status.png" : "./assets/Closed- Status .png"}" alt="">
 
-//                     <button class="btn btn-soft bg-red-100 btn-error rounded-full text-red-500 px-6">${issues.priority}</button>  
+//                     <button class="btn btn-soft bg-red-100 btn-error rounded-full text-red-500 px-6">${issues.priority}</button>
 //                     </div>
 //                         <h4 class="font-semibold text-md">${issues.title}</h4>
 //                         <p class="text-[#64748B]">${issues.description}</p>
@@ -162,13 +167,7 @@ openIssues();
 // openIssues();
 // });
 
-
 // ***********************************************************************
-
-
-
-
-
 
 // const displayOpenIssues=(issueOpen)=>{
 //   issueOpen.forEach((issueOpen)=>{
@@ -185,10 +184,10 @@ openIssues();
 //                 <div id="card" class ="card shadow-lg h-[100%] w-[100%] ">
 //                     <div class="top-part p-4 rounded-md ${borderTop}">
 //                     <div class="top flex justify-between items-center">
-                    
+
 //                   <img src="${issues.status === "open" ? "./assets/Open-Status.png" : "./assets/Closed- Status .png"}" alt="">
 
-//                     <button class="btn btn-soft bg-red-100 btn-error rounded-full text-red-500 px-6">${issues.priority}</button>  
+//                     <button class="btn btn-soft bg-red-100 btn-error rounded-full text-red-500 px-6">${issues.priority}</button>
 //                     </div>
 //                         <h4 class="font-semibold text-md">${issues.title}</h4>
 //                         <p class="text-[#64748B]">${issues.description}</p>
